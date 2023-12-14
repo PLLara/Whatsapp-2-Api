0f3d1b42164803f2417661f696938e5ee9086893
PLLara <freelance.pedrolara@gmail.com>
2023-12-14 16:04:03 -0300

    ':)'

--- build/src/routes/send_message.js
+++ build/src/routes/send_message.js
@@ -16,38 +16,38 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const app_1 = require("firebase/app");
 const database_1 = require("firebase/database");
-router.post('/', function (req, res) {
+router.post("/", function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         var data = new Date();
         const { path, mensagem, usuario, mediaLink } = req.body;
         if (path == undefined || mensagem == undefined || usuario == undefined) {
-            return res.send('Message or path undefined ->' + JSON.stringify(req.body));
+            return res.send("Message or path undefined ->" + JSON.stringify(req.body));
         }
-        if (path == '' || mensagem == undefined || usuario == undefined) {
+        if (path == "" || mensagem == undefined || usuario == undefined) {
             return res.send("Message or path empty ->" + JSON.stringify(req.body));
         }
-        if (typeof path == 'string') {
+        if (typeof path == "string") {
             const firebaseApp = (0, app_1.initializeApp)({
-                apiKey: "AIzaSyBGk-UQzUi87TkW-EPv75PXmXiZ_2n2ONU",
-                authDomain: "whatsappi-2.firebaseapp.com",
-                databaseURL: "https://whatsappi-2-default-rtdb.firebaseio.com",
-                projectId: "whatsappi-2",
-                storageBucket: "whatsappi-2.appspot.com",
-                messagingSenderId: "1005267707114",
-                appId: "1:1005267707114:web:991ad2d96cbe856a6d2393",
-                measurementId: "G-9VR129Y8JL"
+                apiKey: "AIzaSyDQ48wcwgtKMgMzvSOKd1IYUGdkQ3XUe7A",
+                authDomain: "clone-do-zap-99767.firebaseapp.com",
+                databaseURL: "https://clone-do-zap-99767-default-rtdb.firebaseio.com",
+                projectId: "clone-do-zap-99767",
+                storageBucket: "clone-do-zap-99767.appspot.com",
+                messagingSenderId: "912678314863",
+                appId: "1:912678314863:web:12f668dc0224a7a9cbbe9a",
+                measurementId: "G-LK68H06P22",
             });
             var db = (0, database_1.getDatabase)(firebaseApp);
-            const parsedPath = path + '/' + Date.now().toString();
-            const eita = data.toLocaleString('pt-br');
-            const tempo = eita.split(' ')[1];
-            const dias = eita.split(' ')[0].split('/').reverse().join('-');
+            const parsedPath = path + "/" + Date.now().toString();
+            const eita = data.toLocaleString("pt-br");
+            const tempo = eita.split(" ")[1];
+            const dias = eita.split(" ")[0].split("/").reverse().join("-");
             const parsedData = dias + " " + tempo;
             var response = yield (0, database_1.set)((0, database_1.ref)(db, parsedPath), {
                 date: parsedData,
-                mediaLink: mediaLink !== null && mediaLink !== void 0 ? mediaLink : '',
+                mediaLink: mediaLink !== null && mediaLink !== void 0 ? mediaLink : "",
                 mensagem: mensagem,
-                usuario: usuario
+                usuario: usuario,
             });
             return res.send(response);
         }
--- src/routes/send_message.ts
+++ src/routes/send_message.ts
@@ -1,60 +1,50 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 const router = express.Router();
 
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get, set } from "firebase/database";
-import { cachedDataVersionTag } from 'v8';
+import { getDatabase, ref, set } from "firebase/database";
 
+router.post("/", async function (req: Request, res: Response) {
+  var data = new Date();
+  const { path, mensagem, usuario, mediaLink } = req.body;
+  if (path == undefined || mensagem == undefined || usuario == undefined) {
+    return res.send("Message or path undefined ->" + JSON.stringify(req.body));
+  }
+  if (path == "" || mensagem == undefined || usuario == undefined) {
+    return res.send("Message or path empty ->" + JSON.stringify(req.body));
+  }
 
+  if (typeof path == "string") {
+    const firebaseApp = initializeApp({
+      apiKey: "AIzaSyDQ48wcwgtKMgMzvSOKd1IYUGdkQ3XUe7A",
+      authDomain: "clone-do-zap-99767.firebaseapp.com",
+      databaseURL: "https://clone-do-zap-99767-default-rtdb.firebaseio.com",
+      projectId: "clone-do-zap-99767",
+      storageBucket: "clone-do-zap-99767.appspot.com",
+      messagingSenderId: "912678314863",
+      appId: "1:912678314863:web:12f668dc0224a7a9cbbe9a",
+      measurementId: "G-LK68H06P22",
+    });
+    var db = getDatabase(firebaseApp);
 
-router.post('/', async function (req: Request, res: Response) {
-    var data = new Date();
-    const { path, mensagem, usuario, mediaLink } = req.body;
-    if (path == undefined || mensagem == undefined || usuario == undefined) {
-        return res.send('Message or path undefined ->' + JSON.stringify(req.body));
-    }
-    if (path == '' || mensagem == undefined || usuario == undefined) {
-        return res.send("Message or path empty ->" + JSON.stringify(req.body));
-    }
+    const parsedPath = path + "/" + Date.now().toString();
 
+    const eita = data.toLocaleString("pt-br");
+    const tempo = eita.split(" ")[1];
+    const dias = eita.split(" ")[0].split("/").reverse().join("-");
 
-    if (typeof path == 'string') {
-        const firebaseApp = initializeApp({
-            apiKey: "AIzaSyBGk-UQzUi87TkW-EPv75PXmXiZ_2n2ONU",
-            authDomain: "whatsappi-2.firebaseapp.com",
-            databaseURL: "https://whatsappi-2-default-rtdb.firebaseio.com",
-            projectId: "whatsappi-2",
-            storageBucket: "whatsappi-2.appspot.com",
-            messagingSenderId: "1005267707114",
-            appId: "1:1005267707114:web:991ad2d96cbe856a6d2393",
-            measurementId: "G-9VR129Y8JL"
-        });
-        var db = getDatabase(firebaseApp);
+    const parsedData = dias + " " + tempo;
 
-        const parsedPath = path + '/' + Date.now().toString();
-
-        const eita = data.toLocaleString('pt-br');
-        const tempo = eita.split(' ')[1]
-        const dias = eita.split(' ')[0].split('/').reverse().join('-')
-
-        const parsedData = dias + " " + tempo
-
-
-
-        var response = await set(ref(db, parsedPath), {
-            date: parsedData,
-            mediaLink: mediaLink ?? '',
-            mensagem: mensagem,
-            usuario: usuario
-        });
-
-        return res.send(response);
-    } else {
-        return res.send("Unspected error")
-    }
-
+    var response = await set(ref(db, parsedPath), {
+      date: parsedData,
+      mediaLink: mediaLink ?? "",
+      mensagem: mensagem,
+      usuario: usuario,
+    });
+    return res.send(response);
+  } else {
+    return res.send("Unspected error");
+  }
 });
 
 export default router;
-
-
